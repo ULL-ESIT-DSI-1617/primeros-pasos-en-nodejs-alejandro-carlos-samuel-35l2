d8556c280787e2220c62686d1029a0700a64205f
Carlos de Armas <alu0100816167@ull.edu.es>
2017-02-18 19:01:01 +0000

    Añadido gulpgile.js con la tarea build

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var ghPages = require('gulp-gh-pages');
+var shell = require('gulp-shell')
+
+
+
+gulp.task('build', shell.task([
+  'gitbook build txt/ gh-pages/'
+]));
