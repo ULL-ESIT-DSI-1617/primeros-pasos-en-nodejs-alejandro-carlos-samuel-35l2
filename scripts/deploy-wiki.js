a1f09bb745688129f2dbbcb8748c4506409233cb
Samuel <samuvalcarcel@gmail.com>
2017-02-25 19:43:24 +0000

    Terminando deploy-wiki.js

--- scripts/deploy-wiki.js
+++ scripts/deploy-wiki.js
@@ -1,3 +1,23 @@
+require('shelljs/global');
 
+let repo = require('./package.json').repository.wiki;
 
 console.log("Deploy wiki on GitHub");
+
+cd('wiki');
+
+//Remove possible existing git repo. We'll replace entire wiki
+rm('-rf','.git');
+
+require('simple-git')()
+  //Init new repo
+  .init()
+  //The first and only commit to this new Git repo contains all the
+  //files present with the commit message "Deploy to GitHub Pages".
+  .add('./*')
+  .commit("Deploy to Wiki")
+  //Add origin
+  .addRemote('origin','repo')
+  //Force push from the current repo's master branch to the remote
+  //(All previous history on the master branch will be lost, since we are overwriting it.)
+  .push(['-f','origin','master'], function() { console.log("Pushed successfully to" + repo)});
